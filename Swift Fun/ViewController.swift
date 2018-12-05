c69c8afc88b82d2cca20197c5eed4c89ac19efc2
Christian White <christianwhite@christians-air>
2018-12-05 17:02:58 -0500

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Christian White on 12/5/18.
+//  Copyright © 2018 Christian White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
